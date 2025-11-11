c064c92e4f5ce92c6360ffd81525ac7bcfe1829b
oscxr <oruiz5761@gmail.com>
2025-11-11 01:28:47 -0600

    Usuario
    correccion de logout y rutas

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 //Usuario
-Route::post('register', [UsuariosController::class, 'register']);
+Route::post('/login', [UsuariosController::class, 'login']);
+Route::post('/register', [UsuariosController::class, 'register']);
+Route::get('/logout', [UsuariosController::class, 'logout'])->middleware('auth:sanctum');
